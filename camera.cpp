bc64f261c040338b3abb1ef0bb38aad391dbe63b
Luciditay <yvan.smorag@orange.fr>
2021-05-29 06:18:27 +0200

    Debut camera

--- camera.cpp
+++ camera.cpp
@@ -1,5 +1,6 @@
 #include "camera.h"
 #include "math.h"
+#include "quadtree.h"
 
 Point2D add2DVect(Point2D p1, Point2D p2){
     Point2D add = {
@@ -12,7 +13,7 @@ Point2D add2DVect(Point2D p1, Point2D p2){
 Point2D scale2DVect(Point2D p1, float scalaire){
     return {
         p1.x/scalaire,
-        p1.y/slaire,ca
+        p1.y/scalaire
     };
 }
 
@@ -21,8 +22,8 @@ Point2D projection2D(Point3D p1){
     return {p1.x, p1.y};
 }
 
-float norme2D(float x, float y){
-    return sqrt(x*x + y*y);
+float norme2D(Point2D p1){
+    return sqrt(p1.x*p1.x + p1.y * p1.y);
 }
 
 float norme3D(float x, float y, float z){
@@ -34,7 +35,7 @@ float norme3D(float x, float y, float z){
 //La camera est l'origine du repère
 // Ca sert à rien pour l'instant 
 void vecteurCamera(Pointf3D pointRegard, Pointf3D* vecteurUP, Pointf3D* lookAt){
-    float ro = norme(pointRegard.x, pointRegard.y, pointRegard.z);
+    float ro = norme3D(pointRegard.x, pointRegard.y, pointRegard.z);
     float theta = acos(pointRegard.z/ro);
     float phi = atan(pointRegard.y/pointRegard.x);
 
@@ -50,12 +51,12 @@ void vecteurCamera(Pointf3D pointRegard, Pointf3D* vecteurUP, Pointf3D* lookAt){
 }
 
 //Vive la Trigo !
-Triangle* triangleCamera(Pointf3D camPos, Pointf3D vectCamera, float fov, float zFar){
+Triangle* triangleCamera(Point3D camPos, Point3D vectCamera, float fov, float zFar){
     Point2D projection2DVectCamera = projection2D(vectCamera); //==> (a, b)
     Point2D projection2DcamPos = projection2D(camPos); 
     Point2D normaleVectCamera = {-vectCamera.y, vectCamera.x}; //==> (-b, a)
 
-    float scalaire = (tan(fov/2)*zFar)/(norme2(normaleVectCamera));
+    float scalaire = (tan(fov/2)*zFar)/(norme2D(normaleVectCamera));
     Point2D normalScalaire = scale2DVect(normaleVectCamera, scalaire);
     Point2D normalScalaire2 = scale2DVect(normaleVectCamera, -scalaire);
     
